5c869e4b53f0df0a71dc587992fd9af7bedac550
Mathias Vagni <mathiasvagni@googlemail.com>
2022-10-22 23:20:41 +0200

    update example

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,17 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { ResponseBody } from '../src/models';
 import { subscriptionStatus } from '../src/cards/subscriptionStatus';
-import { adminLinks } from '../src/cards/adminLinks';
+import { usefulLinks } from '../src/cards/usefulLinks';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  console.log(`Request received:`);
+  console.log(`Received request:`);
   console.log({
     body: req.body,
     headers: req.headers,
   });
 
   const response: ResponseBody = {
-    cards: [subscriptionStatus, adminLinks],
+    cards: [subscriptionStatus, usefulLinks],
   };
 
   res.status(200).json(response);
---
+++ src/cards/usefulLinks.ts
@@ -0,0 +1,14 @@
+import { Card } from '../models';
+
+export const usefulLinks: Card = {
+  key: 'useful-links',
+  timeToLiveSeconds: null,
+  components: [
+    {
+      componentLinkButton: {
+        linkButtonLabel: 'View in admin',
+        linkButtonUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      },
+    },
+  ],
+};
